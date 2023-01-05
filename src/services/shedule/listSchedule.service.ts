6d714ac44762df4ef31a1bd9ea90d0e06f62ae02
Paulo Vitor <patoassado@gmail.com>
2023-01-05 11:14:00 -0300

    clean code

--- src/services/shedule/listSchedule.service.ts
+++ src/services/shedule/listSchedule.service.ts
@@ -14,17 +14,6 @@ const listSheduleService = async(propertieId:string)=>{
     if(!propertyExist){
         throw new AppError('Property with invalid id', 404)
     }
-    /* const schedules = await propertiesModel.find(
-        {
-            relations: { schedules_users_properties: true }, 
-            where: {id: propertieId}
-        }) */
-
-    /* const schedules = await AppDataSource
-    .getRepository(Schedules_users_properties)
-    .createQueryBuilder("schedules_users_properties")
-    .where("schedules_users_properties.properties = :id", { id: propertieId })
-    .getMany() */
 
     const schedules = await chedulesModel.createQueryBuilder('schedules_users_properties')
     .innerJoinAndSelect('schedules_users_properties.properties', 'properties')
@@ -35,13 +24,6 @@ const listSheduleService = async(propertieId:string)=>{
             'schedules_users_properties.hour as hour', 
             'schedules_users_properties.id as id'])
     .getRawMany()
-
-   /*  const technologies = await projectRepository.createQueryBuilder('projects').
-    innerJoinAndSelect('projects.technologiesToProjects', 'technologiesToProjects').
-    innerJoinAndSelect('technologiesToProjects.technology', 'technologies').
-    where('projects.id = :id_projeto', {id_projeto: projectId}).
-    select(['projects.id as id_projeto', 'technologiesToProjects', 'technologies']).
-    getRawMany() */
 
     return ({schedules: schedules})
 }
